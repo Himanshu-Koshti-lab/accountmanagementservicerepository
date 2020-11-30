7634a5b58f9189882b4727425bd7c36f335590d6
Himanshu Koshti <Himfeedback74@gmail.com>
2020-11-30 22:54:40 +0530

    Same resource

--- src/main/java/com/tcs/poc/app/config/ResourceServerConfig.java
+++ src/main/java/com/tcs/poc/app/config/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.tcs.poc.app.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -14,36 +13,23 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 /*@Configuration*/
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
-	
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
 	@Autowired
 	private Environment env;
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.resourceId(env.getProperty("security.oauth2.client.clientId"));
-		
+
 	}
-	
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-//		http
-//			.csrf()
-//				.disable()
-//			.formLogin()
-//				.disable()
-//			.httpBasic()
-//				.disable()
-//			.sessionManagement()
-//			.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-//			.and()
-//			.authorizeRequests()
-//			.anyRequest()
-//			.authenticated();
 		http
 		.csrf().disable()
 		.authorizeRequests()
-		.antMatchers("/**").permitAll()		
+		.antMatchers("/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.cors();
